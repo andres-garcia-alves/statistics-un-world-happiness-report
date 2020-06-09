45ea7e43d71f952b0835c1e05dc14fc5d18158c0
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2020-06-09 18:07:32 -0300

    new pie-chart

--- Taller II - Script #02.py
+++ Taller II - Script #02.py
@@ -12,18 +12,18 @@ dsFelicidad = pd.read_csv('datasets\\encuesta_felicidad_2015.csv', encoding='utf
 dsAlfabetiz = pd.read_csv('datasets\\alfabetizacion_v04.csv', encoding='ISO-8859-1')
 
 # eliminar filas y columnas innecesarias
-dsFelicidad = dsFelicidad.drop(['ErrorStd','Libertad','Instituciones','Generosidad','Justicia'], axis=1)
+dsFelicidad = dsFelicidad.drop(['ErrorStd'], axis=1)
 dsAlfabetiz = dsAlfabetiz[dsAlfabetiz['Operacion'] != 'IGNORAR']
 dsAlfabetiz = dsAlfabetiz.drop(['Operacion','Fuente','Anio','Mujeres','Hombres','Jovenes','Variacion'], axis=1)
 dsAlfabetiz = dsAlfabetiz.rename(columns={'Adultos':'Alfabetiz'})
 
 # combinar los datos
-dsCombined = pd.merge(left=dsFelicidad, right=dsAlfabetiz, left_on='Pais', right_on='Pais')
-dsCombined.dropna(inplace = True)
+dsCombinado = pd.merge(left=dsFelicidad, right=dsAlfabetiz, left_on='Pais', right_on='Pais')
+dsCombinado.dropna(inplace = True)
 
 # normalizar los datos (rango de 0 a 1)
-dsCombined.PuntajeFelicidad = dsCombined.PuntajeFelicidad / max(dsCombined.PuntajeFelicidad)
-dsCombined.head()
+dsCombinado.PuntajeFelicidad = dsCombinado.PuntajeFelicidad / max(dsCombinado.PuntajeFelicidad)
+dsCombinado.head(15)
 
 
 # %%: visualizar mediante histogramas
@@ -31,12 +31,12 @@ plt.figure(figsize=(10,7))
 plt.xticks(rotation=90)
 
 plt.title("Porcentajes de Alfabetización")
-plt.hist(dsCombined.Alfabetiz, bins=10)
+plt.hist(dsCombinado.Alfabetiz, bins=10)
 plt.show()
 
 
 # %%: valores atípicos: boxplot
-series = [dsCombined.PuntajeFelicidad, dsCombined.Alfabetiz]
+series = [dsCombinado.PuntajeFelicidad, dsCombinado.Alfabetiz]
 labels = ['Felicidad', 'Alfabetizacion']
 
 plt.figure(figsize=(10,6))
@@ -47,13 +47,13 @@ plt.show()
 # %%: valores atípicos: gráfico violín
 plt.figure(figsize=(10,6))
 plt.title("Alfabetizacion")
-sns.violinplot(dsCombined.Alfabetiz, inner="points")
+sns.violinplot(dsCombinado.Alfabetiz, inner="points")
 plt.show()
 
 
 # %%: diagrama de pares (posibles correlaciones)
 vars=['PuntajeFelicidad','Economia','Familia','ExpectVida','Alfabetiz']
-sns.pairplot(dsCombined, vars=vars)
+sns.pairplot(dsCombinado, vars=vars)
 plt.show()
 
 
@@ -61,28 +61,57 @@ plt.show()
 plt.title('Alfabetización vs Felicidad')
 plt.xlabel('Alfabetiz')
 plt.ylabel('Felicidad')
-plt.scatter(dsCombined.Alfabetiz, dsCombined.PuntajeFelicidad, color='black')
+plt.scatter(dsCombinado.Alfabetiz, dsCombinado.PuntajeFelicidad, color='black')
 plt.show()
 
 
 # %%: cálculo de correlaciones
-AlfabetizFelicidad = np.corrcoef(dsCombined.Alfabetiz, dsCombined.PuntajeFelicidad)[0][1]
+AlfabetizFelicidad = np.corrcoef(dsCombinado.Alfabetiz, dsCombinado.PuntajeFelicidad)[0][1]
 print("Alfabetización y Felicidad:")
 print("Correlación    %.2f" % AlfabetizFelicidad)
 
 
 # %%: diagramas de la regresión: lm (lineal model)
-g = sns.lmplot("Alfabetiz", "PuntajeFelicidad", dsCombined, height=6)
+g = sns.lmplot("Alfabetiz", "PuntajeFelicidad", dsCombinado, height=6)
 g.set_axis_labels("Alfabetización", "Felicidad")
 plt.show()
 
 
 # %%: diagramas de la regresión: joint, subtipo 'reg'
-g = sns.jointplot(dsCombined.Alfabetiz, dsCombined.PuntajeFelicidad, kind="reg", height=8, ratio=3, color="r")
+g = sns.jointplot(dsCombinado.Alfabetiz, dsCombinado.PuntajeFelicidad, kind="reg", height=8, ratio=3, color="r")
 g.set_axis_labels("Alfabetización", "Felicidad")
 plt.show()
 
 
-# %%
-# equivalente a: 'SELECT PuntajeFelicidad, Economia GROUP BY Region'
-# dsInstitucFelicidad = pd.pivot_table(ds2015, index='Region', values=["PuntajeFelicidad", "Economia"])
+#%%: factores de felicidad en la Argentina
+dsArgentina = dsFelicidad[dsFelicidad.Pais == 'Argentina']
+dsArgentina['Otros'] = dsArgentina.PuntajeFelicidad - (dsArgentina.Economia + dsArgentina.Familia + dsArgentina.ExpectVida + dsArgentina.Justicia)
+dsPivot = pd.pivot_table(dsArgentina, index='', values=["Economia","Familia","ExpectVida","Justicia","Otros"])
+
+# gráfico de torta
+plt.figure(figsize=(7,7))
+plt.title('Factores de felicidad en Argentina', fontsize=15)
+plt.pie(dsPivot.values, labels=dsPivot.columns, autopct='%1.1f%%')
+plt.show()
+
+
+# %%: promedio de felicidad por región          FALTA, CONT ACA !!!
+regionList = list(dsCombinado.Region.unique())
+
+promFelicidad = []
+promEconomia = []
+promFamilia = []
+promExpectVida = []
+promAlfabetiz = []
+
+for regionItem in regionList:
+    # seleccionar toda la region
+    region = dsCombinado[dsCombinado.Region == regionItem]
+
+    # calcular promedio
+    promFelicidad.append(sum(region.PuntajeFelicidad) / len(region))
+    promEconomia.append(sum(region.Economia) / len(region))
+    promFamilia.append(sum(region.Familia) / len(region))
+    promExpectVida.append(sum(region.ExpectVida) / len(region))
+    promAlfabetiz.append(sum(region.Alfabetiz) / len(region))
+
